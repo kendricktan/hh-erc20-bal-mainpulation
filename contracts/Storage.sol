831306b14fa10915698608677f8369a715c5b76a
Kendrick Tan <kendricktan0814@gmail.com>
2021-07-03 16:30:15 +1000

    more test

---
+++ contracts/Storage.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Storage {
+    uint256 public a = 500;
+    uint256 public b = 1337;
+}
--- test/storage.test.js
+++ test/storage.test.js
@@ -12,14 +12,18 @@ const setStorageAt = async (address, index, value) => {
   await mine();
 };
 
+const toBytes32 = (bn) => {
+  return ethers.utils.hexlify(ethers.utils.zeroPad(bn.toHexString(), 32));
+};
+
 const DAI_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
 
 // Slot 2 is found via command below
 // npx slot20 balanceOf 0x6b175474e89094c44da98b954eedeac495271d0f 0x5d3a536e4d6dbd6114cc1ead35777bab948e3643
 const DAI_SLOT = 2;
 
-describe("Balance Manipulation", function () {
-  it("Should manipulate the balance of DAI", async function () {
+describe("Storage Manipulation", function () {
+  it("Manipulate the balance of DAI", async function () {
     const Dai = new ethers.Contract(DAI_ADDRESS, erc20Abi, ethers.provider);
     const locallyManipulatedBalance = parseUnits("100000");
 
@@ -37,13 +41,43 @@ describe("Balance Manipulation", function () {
     );
 
     // Manipulate local balance (needs to be bytes32 string)
-    const valBytes32 = ethers.utils.hexlify(
-      ethers.utils.zeroPad(locallyManipulatedBalance.toHexString(), 32)
+    await setStorageAt(
+      DAI_ADDRESS,
+      index.toString(),
+      toBytes32(locallyManipulatedBalance).toString()
     );
-    await setStorageAt(DAI_ADDRESS, index.toString(), valBytes32.toString());
 
     // check post manipulation
     const after = await Dai.balanceOf(userAddress);
     expect(after.eq(locallyManipulatedBalance)).to.be.true;
+  });
+
+  it("Manipulate the Storage Contract state", async function () {
+    const storage = await ethers
+      .getContractFactory("Storage")
+      .then((x) => x.deploy());
+
+    const beforeA = await storage.a();
+    const beforeB = await storage.b();
+
+    expect(beforeA.eq(ethers.BigNumber.from("500"))).to.be.true;
+    expect(beforeB.eq(ethers.BigNumber.from("1337"))).to.be.true;
+
+    await setStorageAt(
+      storage.address,
+      "0x0",
+      toBytes32(ethers.BigNumber.from("42")).toString()
+    );
+    await setStorageAt(
+      storage.address,
+      "0x1",
+      toBytes32(ethers.BigNumber.from("41")).toString()
+    );
+
+    const afterA = await storage.a();
+    const afterB = await storage.b();
+
+    expect(afterA.eq(ethers.BigNumber.from("42"))).to.be.true;
+    expect(afterB.eq(ethers.BigNumber.from("41"))).to.be.true;
   });
 });
